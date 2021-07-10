ef0108be2ef0ce699f70c12733d7f74c2a318541
MICHAEL BAZAN SANCHEZ <mbazansanchez@cnm.edu>
2021-07-09 22:06:59 -0600

    Add project files.

---
+++ StudenFormDemo/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿
+namespace StudenFormDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txbLastName = new System.Windows.Forms.TextBox();
+            this.txbFirstName = new System.Windows.Forms.TextBox();
+            this.txbStudentID = new System.Windows.Forms.TextBox();
+            this.lblStudentID = new System.Windows.Forms.Label();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.gbMajor = new System.Windows.Forms.GroupBox();
+            this.rbIT = new System.Windows.Forms.RadioButton();
+            this.rbWebDesign = new System.Windows.Forms.RadioButton();
+            this.rbProgramming = new System.Windows.Forms.RadioButton();
+            this.cbxCIS101 = new System.Windows.Forms.CheckBox();
+            this.cbxCIS102 = new System.Windows.Forms.CheckBox();
+            this.cbxCIS103 = new System.Windows.Forms.CheckBox();
+            this.cbxCIS104 = new System.Windows.Forms.CheckBox();
+            this.lblCourses = new System.Windows.Forms.Label();
+            this.lblScores = new System.Windows.Forms.Label();
+            this.txbScore1 = new System.Windows.Forms.TextBox();
+            this.txbScore2 = new System.Windows.Forms.TextBox();
+            this.txbscore3 = new System.Windows.Forms.TextBox();
+            this.txbscore4 = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.gbMajor.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txbLastName
+            // 
+            this.txbLastName.Location = new System.Drawing.Point(98, 85);
+            this.txbLastName.Name = "txbLastName";
+            this.txbLastName.Size = new System.Drawing.Size(100, 23);
+            this.txbLastName.TabIndex = 0;
+            this.txbLastName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txbFirstName
+            // 
+            this.txbFirstName.Location = new System.Drawing.Point(98, 58);
+            this.txbFirstName.Name = "txbFirstName";
+            this.txbFirstName.Size = new System.Drawing.Size(100, 23);
+            this.txbFirstName.TabIndex = 2;
+            // 
+            // txbStudentID
+            // 
+            this.txbStudentID.Location = new System.Drawing.Point(98, 34);
+            this.txbStudentID.Name = "txbStudentID";
+            this.txbStudentID.Size = new System.Drawing.Size(100, 23);
+            this.txbStudentID.TabIndex = 3;
+            this.txbStudentID.TextChanged += new System.EventHandler(this.txbStudentID_TextChanged);
+            // 
+            // lblStudentID
+            // 
+            this.lblStudentID.AutoSize = true;
+            this.lblStudentID.Location = new System.Drawing.Point(28, 34);
+            this.lblStudentID.Name = "lblStudentID";
+            this.lblStudentID.Size = new System.Drawing.Size(59, 15);
+            this.lblStudentID.TabIndex = 5;
+            this.lblStudentID.Text = "StudentID";
+            this.lblStudentID.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Location = new System.Drawing.Point(28, 61);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(61, 15);
+            this.lblFirstName.TabIndex = 6;
+            this.lblFirstName.Text = "FirstName";
+            this.lblFirstName.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(-1198, -238);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "label4";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(28, 85);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(60, 15);
+            this.lblLastName.TabIndex = 8;
+            this.lblLastName.Text = "LastName";
+            this.lblLastName.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // gbMajor
+            // 
+            this.gbMajor.Controls.Add(this.rbIT);
+            this.gbMajor.Controls.Add(this.rbWebDesign);
+            this.gbMajor.Controls.Add(this.rbProgramming);
+            this.gbMajor.Location = new System.Drawing.Point(28, 129);
+            this.gbMajor.Name = "gbMajor";
+            this.gbMajor.Size = new System.Drawing.Size(200, 89);
+            this.gbMajor.TabIndex = 9;
+            this.gbMajor.TabStop = false;
+            this.gbMajor.Text = "Major";
+            // 
+            // rbIT
+            // 
+            this.rbIT.AutoSize = true;
+            this.rbIT.Location = new System.Drawing.Point(7, 69);
+            this.rbIT.Name = "rbIT";
+            this.rbIT.Size = new System.Drawing.Size(34, 19);
+            this.rbIT.TabIndex = 2;
+            this.rbIT.TabStop = true;
+            this.rbIT.Text = "IT";
+            this.rbIT.UseVisualStyleBackColor = true;
+            // 
+            // rbWebDesign
+            // 
+            this.rbWebDesign.AutoSize = true;
+            this.rbWebDesign.Location = new System.Drawing.Point(6, 49);
+            this.rbWebDesign.Name = "rbWebDesign";
+            this.rbWebDesign.Size = new System.Drawing.Size(88, 19);
+            this.rbWebDesign.TabIndex = 1;
+            this.rbWebDesign.TabStop = true;
+            this.rbWebDesign.Text = "Web Design";
+            this.rbWebDesign.UseVisualStyleBackColor = true;
+            // 
+            // rbProgramming
+            // 
+            this.rbProgramming.AutoSize = true;
+            this.rbProgramming.Location = new System.Drawing.Point(7, 24);
+            this.rbProgramming.Name = "rbProgramming";
+            this.rbProgramming.Size = new System.Drawing.Size(99, 19);
+            this.rbProgramming.TabIndex = 0;
+            this.rbProgramming.TabStop = true;
+            this.rbProgramming.Text = "Programming";
+            this.rbProgramming.UseVisualStyleBackColor = true;
+            this.rbProgramming.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // cbxCIS101
+            // 
+            this.cbxCIS101.AutoSize = true;
+            this.cbxCIS101.Location = new System.Drawing.Point(28, 283);
+            this.cbxCIS101.Name = "cbxCIS101";
+            this.cbxCIS101.Size = new System.Drawing.Size(64, 19);
+            this.cbxCIS101.TabIndex = 10;
+            this.cbxCIS101.Text = "CIS 101";
+            this.cbxCIS101.UseVisualStyleBackColor = true;
+            this.cbxCIS101.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // cbxCIS102
+            // 
+            this.cbxCIS102.AutoSize = true;
+            this.cbxCIS102.Location = new System.Drawing.Point(28, 308);
+            this.cbxCIS102.Name = "cbxCIS102";
+            this.cbxCIS102.Size = new System.Drawing.Size(64, 19);
+            this.cbxCIS102.TabIndex = 11;
+            this.cbxCIS102.Text = "CIS 102";
+            this.cbxCIS102.UseVisualStyleBackColor = true;
+            // 
+            // cbxCIS103
+            // 
+            this.cbxCIS103.AutoSize = true;
+            this.cbxCIS103.Location = new System.Drawing.Point(28, 333);
+            this.cbxCIS103.Name = "cbxCIS103";
+            this.cbxCIS103.Size = new System.Drawing.Size(64, 19);
+            this.cbxCIS103.TabIndex = 12;
+            this.cbxCIS103.Text = "CIS 103";
+            this.cbxCIS103.UseVisualStyleBackColor = true;
+            // 
+            // cbxCIS104
+            // 
+            this.cbxCIS104.AutoSize = true;
+            this.cbxCIS104.Location = new System.Drawing.Point(28, 358);
+            this.cbxCIS104.Name = "cbxCIS104";
+            this.cbxCIS104.Size = new System.Drawing.Size(64, 19);
+            this.cbxCIS104.TabIndex = 13;
+            this.cbxCIS104.Text = "CIS 104";
+            this.cbxCIS104.UseVisualStyleBackColor = true;
+            // 
+            // lblCourses
+            // 
+            this.lblCourses.AutoSize = true;
+            this.lblCourses.Location = new System.Drawing.Point(28, 256);
+            this.lblCourses.Name = "lblCourses";
+            this.lblCourses.Size = new System.Drawing.Size(49, 15);
+            this.lblCourses.TabIndex = 14;
+            this.lblCourses.Text = "Courses";
+            // 
+            // lblScores
+            // 
+            this.lblScores.AutoSize = true;
+            this.lblScores.Location = new System.Drawing.Point(168, 256);
+            this.lblScores.Name = "lblScores";
+            this.lblScores.Size = new System.Drawing.Size(41, 15);
+            this.lblScores.TabIndex = 15;
+            this.lblScores.Text = "Scores";
+            this.lblScores.Click += new System.EventHandler(this.lblScores_Click);
+            // 
+            // txbScore1
+            // 
+            this.txbScore1.Location = new System.Drawing.Point(158, 283);
+            this.txbScore1.Name = "txbScore1";
+            this.txbScore1.Size = new System.Drawing.Size(100, 23);
+            this.txbScore1.TabIndex = 16;
+            // 
+            // txbScore2
+            // 
+            this.txbScore2.Location = new System.Drawing.Point(158, 308);
+            this.txbScore2.Name = "txbScore2";
+            this.txbScore2.Size = new System.Drawing.Size(100, 23);
+            this.txbScore2.TabIndex = 17;
+            // 
+            // txbscore3
+            // 
+            this.txbscore3.Location = new System.Drawing.Point(158, 333);
+            this.txbscore3.Name = "txbscore3";
+            this.txbscore3.Size = new System.Drawing.Size(100, 23);
+            this.txbscore3.TabIndex = 18;
+            // 
+            // txbscore4
+            // 
+            this.txbscore4.Location = new System.Drawing.Point(158, 358);
+            this.txbscore4.Name = "txbscore4";
+            this.txbscore4.Size = new System.Drawing.Size(100, 23);
+            this.txbscore4.TabIndex = 19;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(28, 400);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 20;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txbscore4);
+            this.Controls.Add(this.txbscore3);
+            this.Controls.Add(this.txbScore2);
+            this.Controls.Add(this.txbScore1);
+            this.Controls.Add(this.lblScores);
+            this.Controls.Add(this.lblCourses);
+            this.Controls.Add(this.cbxCIS104);
+            this.Controls.Add(this.cbxCIS103);
+            this.Controls.Add(this.cbxCIS102);
+            this.Controls.Add(this.cbxCIS101);
+            this.Controls.Add(this.gbMajor);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.lblStudentID);
+            this.Controls.Add(this.txbStudentID);
+            this.Controls.Add(this.txbFirstName);
+            this.Controls.Add(this.txbLastName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.gbMajor.ResumeLayout(false);
+            this.gbMajor.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txbLastName;
+        private System.Windows.Forms.TextBox txbFirstName;
+        private System.Windows.Forms.TextBox txbStudentID;
+        private System.Windows.Forms.Label lblStudentID;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.GroupBox gbMajor;
+        private System.Windows.Forms.RadioButton rbIT;
+        private System.Windows.Forms.RadioButton rbWebDesign;
+        private System.Windows.Forms.RadioButton rbProgramming;
+        private System.Windows.Forms.CheckBox cbxCIS101;
+        private System.Windows.Forms.CheckBox cbxCIS102;
+        private System.Windows.Forms.CheckBox cbxCIS103;
+        private System.Windows.Forms.CheckBox cbxCIS104;
+        private System.Windows.Forms.Label lblCourses;
+        private System.Windows.Forms.Label lblScores;
+        private System.Windows.Forms.TextBox txbScore1;
+        private System.Windows.Forms.TextBox txbScore2;
+        private System.Windows.Forms.TextBox txbscore3;
+        private System.Windows.Forms.TextBox txbscore4;
+        private System.Windows.Forms.Button btnSubmit;
+    }
+}
+
---
+++ StudenFormDemo/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudenFormDemo
+{
+    public partial class Form1 : Form
+    {
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void lblScores_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            String major = "TBH";
+            if(rbProgramming.Checked)
+            {
+                major = rbProgramming.Text;
+            } else if (rbWebDesign.Checked)
+            {
+                major = rbWebDesign.Text;
+            } else if (rbIT.Checked)
+            {
+                major = rbIT.Text;
+            }
+
+
+
+        }
+
+        private void txbStudentID_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
